f0f154616530ee312d9feecf9d07d16dc153ddbe
Ashleigh Maund <ashleighmaund@BCSG-M015.local>
2018-10-26 09:52:07 +0100

    google oauth route and callback set up

--- index.js
+++ index.js
@@ -2,12 +2,28 @@ const express = require('express');
 const passport = require('passport');
 // only want to use Strategy from passport-google-oauth20
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys');
 const app = express();
 
-passport.use(new GoogleStrategy());
+passport.use(
+	// GoogleStrategy has internal identifier 'google'
+	// Want to ask for 'profile' and 'email' (see below)
+	new GoogleStrategy({
+		clientID: keys.googleClientID,
+		clientSecret: keys.googleClientSecret,
+		callbackUrl: '/auth/google/callback'
+	}, (accessToken) => {
+		console.log(accessToken);
+	})
+);
 
-app.get('/', (req, res) => {
-  res.send({hi: 'there'});
-});
+app.get(
+	'/auth/google',
+	passport.authenticate('google', {
+	    scope: ['profile', 'email']
+    })
+);
 
-app.listen(5000);
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT);
